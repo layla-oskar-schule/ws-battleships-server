aac11aed5a30afc8a2c714b81f531c376d43b63a
oskar <36141470+devskar@users.noreply.github.com>
2022-11-26 11:58:38 +0100

    Rename ServerEvents/MessageEvent.cs to Server/Events/MessageEvent.cs

---
+++ Server/Events/MessageEvent.cs
@@ -0,0 +1,20 @@
+﻿using server.Game.Controllers;
+using server.Handlers;
+using server.SocketManager;
+using System.Net.WebSockets;
+
+
+namespace server.Events
+{
+    public abstract class MessageEvent
+    {
+        public string Name { get; private set; }
+
+        public MessageEvent(string name) 
+        {
+            Name = name;
+        }
+
+        public abstract Task OnEvent( SocketHandler handler, GamesController gamesController, ConnectionManager connections, WebSocket sender, string message);
+    }
+}
