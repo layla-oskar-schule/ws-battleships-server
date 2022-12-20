229825cf3bf429b22edaa24db952ddca77adfd09
llayla <layla.shokri@gmx.de>
2022-12-20 10:44:52 +0100

    added on send fire event

---
+++ Server/Events/OnSendFireLocationEvent.cs
@@ -0,0 +1,42 @@
+﻿using Lib.Constants;
+using Lib.GameEntities;
+using Newtonsoft.Json;
+using server.Game.Controllers;
+using server.Game.Entities;
+using server.Handlers;
+
+namespace Server.Events
+{
+    public class OnSendFireLocationEvent : GameMessageEvent
+    {
+        public OnSendFireLocationEvent() : base(EventName.SendFireLocationEvent) { }
+
+        public override async Task OnGameEvent(SocketHandler handler, GamesController gamesController, Player player, string message)
+        {
+            if (String.IsNullOrWhiteSpace(message))
+            {
+                await player.SendMessage(EventName.SendMessageEvent + EventName.SUFFIX + "You need to provide a valid location");
+                await player.SendMessage(EventName.SendFireLocationEvent + EventName.SUFFIX);
+                return;
+            }
+            string[] tmp = JsonConvert.DeserializeObject<string[]>(message)!;
+            Location location = Location.FromString(tmp[0]);
+            BattleshipsGame game = gamesController.GetGameByPlayer(player);
+            bool IsHit = game!.Shoot(player, gamesController.GetGameByPlayer(player).GetOtherPlayer(player), location);
+            if (IsHit)
+            {
+                await player.SendMessage(EventName.SendMessageEvent + EventName.SUFFIX + "Hit! Shoot one more time");
+                await player.SendMessage(EventName.SendBoatLocationEvent + EventName.SUFFIX);
+                if (!gamesController.CheckIfOpponentHasBoatsLeft(player))
+                {
+                    await player.SendMessage(EventName.SendMessageEvent + EventName.SUFFIX + "Game won!");
+                    // stop game
+                }
+            } else
+            {
+                await player.SendMessage(EventName.SendMessageEvent + EventName.SUFFIX + "No hit, other players turn");
+                // next step
+            }
+        }
+    }
+}
